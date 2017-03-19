d5917b875bdc7fdc46c2bef34c4c4dd56e3ca5ff
Jarrett Tolman <jarrett.tolman@gmail.com>
2017-03-19 14:34:45 -0400

    reorganized folders closer to mvc, part 1

--- controllers/index.js
+++ controllers/index.js
@@ -6,4 +6,7 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET users using users.js */
+router.use('/users', require('./users'));
+
 module.exports = router;
